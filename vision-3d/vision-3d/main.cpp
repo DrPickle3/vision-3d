1ce02398a12f0d0ee4faacbcd4b283063d21f98b
DrPickle3 <ronc1402@usherbrooke.ca>
2025-05-26 15:30:44 -0400

    meilleur cible

--- vision-3d/vision-3d/main.cpp
+++ vision-3d/vision-3d/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+#include <opencv2/opencv.hpp>
 #include <vector>
-#include <iomanip>
 
 using namespace std;
+using namespace cv;
 
-int main() {
-	cout << "Hello World !";
-	return 0;
+int main()
+{
+	Mat image = imread("images/test2.jpg");
+
+    if (image.empty()) {
+        cout << "Could not open or find the image!" << endl;
+        return -1;
+    }
+
+    Mat gray;
+    cvtColor(image, gray, COLOR_BGR2GRAY); // Convert to grayscale
+
+    vector<Point2f> corners;
+	int maxCorners = 750;
+	double qualityLevel = 0.1; 
+	double minDistance = 100;  
+    goodFeaturesToTrack(gray, corners, maxCorners, qualityLevel, minDistance);
+
+    // Draw the detected corners as green circles
+    for (const Point2f& pt : corners) {
+        circle(image, pt, 5, Scalar(0, 255, 0), -1); // Green circle
+    }
+
+    imwrite("corners.png", image);
+    waitKey(0);
+	cout << corners.size();
+    return 0;
 }

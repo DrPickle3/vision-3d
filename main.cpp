93b7e417cc5851b89040b949d151ef6d2e904d9a
bayatriss <152737348+bayatriss@users.noreply.github.com>
2025-06-04 12:17:26 -0400

    Ordonner les points en colonne

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 using namespace cv;
@@ -22,7 +23,7 @@ int main()
     vector<Point2f> corners;
 	int maxCorners = 750;
 	double qualityLevel = 0.03;
-	double minDistance = 70;
+	double minDistance = 70;  
     goodFeaturesToTrack(gray, corners, maxCorners, qualityLevel, minDistance);
 
     for (const Point2f& pt : corners) {
@@ -32,5 +33,25 @@ int main()
     imwrite("corners.png", image);
     waitKey(0);
 	cout << corners.size() << endl;
+      // 1) Trier les points par x, puis par y
+    std::sort(corners.begin(), corners.end(),
+        [](const Point2f &a, const Point2f &b) {
+            if (std::fabs(a.x - b.x) > 1e-3)
+                return a.x < b.x;
+            return a.y < b.y;
+        });
+
+    // 2) Écrire dans corners.txt une fois trié
+    ofstream file("corners.txt");
+    if (!file.is_open()) {
+        cout << "Could not open the file!" << endl;
+        return -1;
+    }
+    for (const Point2f& pt : corners) {
+        file << pt.x << "," << pt.y << "," << "0" << endl;
+    }
+    file.close();
+
+
     return 0;
 }

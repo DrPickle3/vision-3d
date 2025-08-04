0ab65c45acb87126e1b4af7e982fb3f089a3f16f
roxylamini <roxylamini13@outlook.com>
2025-08-04 09:01:27 -0400

    tout fait mais je dois avouer que la contrainte de continuité ej la comprends pas a 110%

--- Dev3/main.cpp
+++ Dev3/main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/viz.hpp>
 
 using namespace cv;
 using namespace std;
@@ -21,7 +22,7 @@ Mat correlationSSD(const Mat& gauche, const Mat& droite, int tailleFenetre, int 
         for (int x = halfWindow; x < numberOfColumns - halfWindow; ++x)
         {
             int meilleureDisparite = 0;
-            double meilleurSSD = DBL_MAX;
+            double meilleurScore = DBL_MAX;
 
             // Contrainte ordre
             // On vérifie la disparité précédente pour éviter les sauts importants
@@ -52,9 +53,29 @@ Mat correlationSSD(const Mat& gauche, const Mat& droite, int tailleFenetre, int 
                     }
                 }
 
-                if (ssd < meilleurSSD)
+                //Contrainte de continuite selon ce que je comprends
+                double penaliteContinuite = 0.0;
+                // Seuil de continuité
+                int epsilon = 3;
+
+                // Comparer avec le pixel de gauche
+                if (x > halfWindow)
                 {
-                    meilleurSSD = ssd;
+                    int dispVoisin = img_disparite.at<uchar>(y, x - 1);
+                    penaliteContinuite += abs(disp - dispVoisin) > epsilon ? 50.0 : 0.0;
+                }
+                // Comparer avec le pixel du dessus
+                if (y > halfWindow)
+                {
+                    int dispVoisin = img_disparite.at<uchar>(y - 1, x);
+                    penaliteContinuite += abs(disp - dispVoisin) > epsilon ? 50.0 : 0.0;
+                }
+
+                double score = ssd + penaliteContinuite;
+
+                if (score < meilleurScore)
+                {
+                    meilleurScore = score;
                     meilleureDisparite = disp;
                 }
             }
@@ -65,9 +86,68 @@ Mat correlationSSD(const Mat& gauche, const Mat& droite, int tailleFenetre, int 
     return img_disparite;
 }
 
+Mat calculerProfondeur(const Mat& disparite)
+{
+    double zPrime = 1500.0;
+    double Tx = 80.0;
+    double dOx = 52.0;
 
-// tailleFenetre : similitude
-// maxDisparite  : limite de la disparité
+    Mat carteProfondeur(disparite.size(), CV_32F, Scalar(0));
+
+    for (int y = 0; y < disparite.rows; ++y)
+    {
+        for (int x = 0; x < disparite.cols; ++x)
+        {
+            int disp = disparite.at<uchar>(y, x);
+
+            if (disp > 0)
+            {
+                float profondeur = static_cast<float>((zPrime * Tx) / (disp + dOx));
+                carteProfondeur.at<float>(y, x) = profondeur;
+            }
+            else
+            {
+                carteProfondeur.at<float>(y, x) = 0.0f;
+            }
+        }
+    }
+
+    return carteProfondeur;
+}
+
+void afficherCarteProfondeur3D(const Mat& carteProfondeur)
+{
+    double focale = 1500.0;
+    double Ox = carteProfondeur.cols / 2.0;
+    double Oy = carteProfondeur.rows / 2.0;
+
+    vector<Vec3f> points;
+
+    // Conversioncarte de profondeur en nuage de points 3D
+    for (int y = 0; y < carteProfondeur.rows; ++y)
+    {
+        for (int x = 0; x < carteProfondeur.cols; ++x)
+        {
+            float z = carteProfondeur.at<float>(y, x);
+
+            if (z > 0) 
+            {
+                float X = (x - Ox) * z / focale;
+                float Y = (Oy - y) * z / focale; // inversion de Y pour l'affichage
+                points.emplace_back(X, Y, -z); // -z pour que les objets proches soient devant
+            }
+        }
+    }
+
+    Mat cloudMat(points.size(), 1, CV_32FC3, points.data());
+
+    viz::Viz3d fenetre("Visualisation 3D de la carte de profondeur");
+    viz::WCloud nuage(cloudMat, viz::Color::green());
+    fenetre.showWidget("nuage", nuage);
+    fenetre.spin();
+}
+
+
 int main()
 {
     Mat imageG = imread("images/im0.png", IMREAD_GRAYSCALE);
@@ -85,18 +165,20 @@ int main()
         return -1;
     }
 
+
     int tailleFenetre = 5;
     int maxDisparite = 64;
 
-    Mat resultat = correlationSSD(imageG, imageD, tailleFenetre, maxDisparite);
+    Mat disparite = correlationSSD(imageG, imageD, tailleFenetre, maxDisparite);
+
+    Mat carteProfondeur = calculerProfondeur(disparite);
+    afficherCarteProfondeur3D(carteProfondeur);
 
     // Seulement contrainte de similitude + limite de la disparité
     Mat carteDisparite;
-    resultat.convertTo(carteDisparite, CV_8U, 255.0 / maxDisparite);
+    disparite.convertTo(carteDisparite, CV_8U, 255.0 / maxDisparite);
     imwrite("./images/carte_disparite_similitude+limite.png", carteDisparite);
     waitKey(10);
-
-
 
     cout << "Hello World!" << endl;
     return 0;
